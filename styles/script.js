9cf70c66203670945738afcce0ae67474b1c3544
Woutnak <kantoramanandraibe01@gmail.com>
2025-07-09 09:22:36 +0300

    responsive desing

--- styles/script.js
+++ styles/script.js
@@ -1,7 +1,8 @@
+const burger = document.querySelector('.burger');
+  const navLinks = document.querySelector('.nav-links');
 
-const navbar = document.getElementById('Navbar');
-
-navbar.addEventListener('mouseout', () =>{
-    const bg = navbar.dataset.bg;
-    navbar.style.backgroundColor = bg;
-})
+  burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    // Animation des barres du burger (optionnel)
+    burger.classList.toggle('toggle');
+  });
